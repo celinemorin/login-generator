3ec5877893e715997af3b44a40b5a665ea611570
hanoko <celine.morin.42@gmail.com>
2017-11-29 14:23:10 +0100

    fix #5
    tests termines #3

--- src/test/java/geco/LoginGeneratorTest.java
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -6,20 +6,21 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class LoginGeneratorTest {
+
     @Test
     public void generateLoginForNomAndPrenom() throws Exception {
         LoginService loginService = new LoginService(new String[] {"JROL", "BPER", "CGUR", "JDUP", "JRAL", "JRAL1"});
         LoginGenerator loginGenerator = new LoginGenerator(loginService);
         loginGenerator.generateLoginForNomAndPrenom("Durand", "Paul");
         assertThat(loginService.loginExists("PDUR"), is(true));
-        /*loginService = new LoginService(new String[] {"JROL", "BPER", "CGUR", "JDUP", "JRAL", "JRAL1"});
+        loginService = new LoginService(new String[] {"JROL", "BPER", "CGUR", "JDUP", "JRAL", "JRAL1"});
         loginGenerator = new LoginGenerator(loginService);
         loginGenerator.generateLoginForNomAndPrenom("Rolling", "Jean");
         assertThat(loginService.loginExists("JROL1"), is(true));
         loginService = new LoginService(new String[] {"JROL", "BPER", "CGUR", "JDUP", "JRAL", "JRAL1"});
         loginGenerator = new LoginGenerator(loginService);
         loginGenerator.generateLoginForNomAndPrenom("Dùrand", "Paul");
-        assertThat(loginService.loginExists("PDUR"), is(true));*/
+        assertThat(loginService.loginExists("PDUR"), is(true));
     }
 
     @Test
